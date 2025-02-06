a969fb0e65ff1ca1ef79788507ea96476f372d5c
sbx_user1051 <sbx_user1051@169.254.5.241>
2025-02-06 17:09:34 +0000

    Thu Feb 06 17:09:34 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -17,12 +17,14 @@ def checkout(skus):
     for item in skus:
         if item not in prices:
             return -1
+        # if item not in items
         items_in_skus[item] += 1
     
     for item in items_in_skus.keys():
         if item in special_prices:
-            while items_in_skus[item] > special_prices[item][0]:
+            while items_in_skus[item] >= special_prices[item][0]:
                 total += special_prices[item][1]
                 items_in_skus[item] -= special_prices[item][0]
         total += prices[item]*items_in_skus[item]
     return total
+
