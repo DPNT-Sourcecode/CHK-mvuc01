375418b8443d5813764823186c681a6a1db535bc
sbx_user1051 <sbx_user1051@169.254.5.241>
2025-02-06 18:17:53 +0000

    Thu Feb 06 18:17:53 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -115,7 +115,9 @@ def buy_x_get_one(x, number, skus):
 def buy_x_get_y(x,offer,skus):
     number, y = offer
     tmp = skus[x]
-    while tmp >= number and skus[y] and skus[y] > 0:
+    if y not in skus:
+        return
+    while tmp >= number and skus[y] > 0:
         skus[y] -=1
         tmp-=number
 
