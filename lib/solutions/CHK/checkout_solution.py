3cd2beed41f7dcd13005ae9d43394582996894d0
sbx_user1051 <sbx_user1051@169.254.5.241>
2025-02-06 18:09:25 +0000

    Thu Feb 06 18:09:25 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,6 +10,7 @@ def checkout(skus):
         "E": 40,
         "F": 10,
     }
+
     # special_prices = {
     #     "A": [3, 130],
     #     "B": [2, 45]
@@ -28,42 +29,73 @@ def checkout(skus):
     return total
 
 def check_offers(items_in_skus):
-        total = 0
+    bulk_buy = {
+        "A": [[5,200], [3,130]],
+        "B": [[2,45]],
+        "H": [[10,80], [5,45]],
+        "K": [[2,150]],
+        "P": [[5,200]],
+        "Q": [[3,80]],
+        "V": [[2,90], [3,130]],
+    }
+
+    bogof = {
+        "F": 2,
+        "U": 3
+    }
+    bogaf = {
+        "E": [2, "B"],
+        "N": [3, "M"],
+        "R": [3, "Q"]
+    }
+    total = 0
+    for item in items_in_skus:
+        if item in bogaf:
+            buy_x_get_y(item, bogaf[item], items_in_skus)
+        if item in bogof:
+            buy_x_get_one(item, bogof[item], items_in_skus)
+        if item in bulk_buy:
+            for number, cost in item[bulk_buy]:
+                total += x_for_y(item, number, cost, items_in_skus)
+
+    return total, items_in_skus
 
 
 
 
 
-        while items_in_skus["A"] >= 5:
-            total += 200
-            items_in_skus["A"] -= 5
-        while items_in_skus["A"] >= 3:
-            total+=130
-            items_in_skus["A"] -= 3
-        tmp = items_in_skus["E"]
-        while tmp >= 2 and items_in_skus["B"]>0:
-            items_in_skus["B"] -=1
-            tmp-=2
-        while items_in_skus["B"] >=2:
-            total += 45
-            items_in_skus["B"] -= 2
-        if items_in_skus["F"] > 2:
-            # get 1/3 of F's removed(rounded down)
-            discount = items_in_skus["F"] // 3
-            items_in_skus["F"] -= discount
-        return total, items_in_skus
+        # while items_in_skus["A"] >= 5:
+        #     total += 200
+        #     items_in_skus["A"] -= 5
+        # while items_in_skus["A"] >= 3:
+        #     total+=130
+        #     items_in_skus["A"] -= 3
+        # tmp = items_in_skus["E"]
+        # while tmp >= 2 and items_in_skus["B"]>0:
+        #     items_in_skus["B"] -=1
+        #     tmp-=2
+        # while items_in_skus["B"] >=2:
+        #     total += 45
+        #     items_in_skus["B"] -= 2
+        # if items_in_skus["F"] > 2:
+        #     # get 1/3 of F's removed(rounded down)
+        #     discount = items_in_skus["F"] // 3
+        #     items_in_skus["F"] -= discount
 
-def x_for_y(item, number, price, total, skus):
-    if skus[item] >= number:
+def x_for_y(item, number, price, skus):
+    total = 0
+    while skus[item] >= number:
         total +=price
         skus[item] -= number
+    return total
 
-def buy_x_get_one(item, number, skus):
-    if skus[item] > number:
-        discount = skus[item] // (number+1)
-        skus[item] -= discount
+def buy_x_get_one(x, number, skus):
+    if skus[x] > number:
+        discount = skus[x] // (number+1)
+        skus[x] -= discount
 
-def buy_x_get_y(x,y,number,skus):
+def buy_x_get_y(x,offer,skus):
+    number, y = offer
     tmp = skus[x]
     while tmp >= number and skus[y]>0:
         skus[y] -=1
